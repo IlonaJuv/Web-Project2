f7452958c2e90153e6663d39d162083385f75fff
IlonaJuv <ilonaju@metropolia.fi>
2023-10-03 13:57:37 +0300

    fixed useerresolver

--- backend/src/api/resolvers/userResolver.ts
+++ backend/src/api/resolvers/userResolver.ts
@@ -1,23 +1,24 @@
 import LoginMessageResponse from "../../interfaces/LoginMessageResponse";
 import fetchData from "../../utils/fetchData";
 import { User } from "../../interfaces/user";
+import AuthMessageResponse from "../../interfaces/AuthMessageResponse";
 
 export default {
     Query: {
         users: async () => {
-              const users = await fetchData<LoginMessageResponse>(
+              const users = await fetchData<AuthMessageResponse>(
                 `${process.env.AUTH_URL}/users`
               );
               console.log("userResolver users: ", users)
-              return users;
+              return users.data;
           },
         userById: async (_parent: undefined, args: {id: string}) => {
             console.log("userbyid")
-            const user = await fetchData<LoginMessageResponse>(
+            const user = await fetchData<AuthMessageResponse>(
               `${process.env.AUTH_URL}/users/${args.id}`
             );
             console.log("userByID, ", user)
-            return user;
+            return user.data;
           },
       },
       Mutation: {
@@ -36,7 +37,7 @@ export default {
             );
             return user;
           },
-          register: async (_parent: undefined, args: {user: Omit<User, 'role'>}) => {
+          register: async (_parent: undefined, args: {user: User}) => {
             console.log("register")
             const options: RequestInit = {
               method: 'POST',
@@ -44,7 +45,7 @@ export default {
               body: JSON.stringify(args.user),
             };
       
-            const user = await fetchData<LoginMessageResponse>(
+            const user = await fetchData<AuthMessageResponse>(
               `${process.env.AUTH_URL}/users`,
               options
             );
