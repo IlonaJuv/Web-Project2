7989994b922ce4440d9a0bd42df73241bf2eeb8b
atteraisanen <atteraisa@gmail.com>
2023-10-11 23:41:35 +0300

    Updated song page styling
    and also made styling more consistent between pages

--- frontend/src/components/ReviewCard/SongPageReview.tsx
+++ frontend/src/components/ReviewCard/SongPageReview.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 interface SongPageReviewProps {
   id: string;
@@ -47,6 +49,7 @@ const SongPageReview: React.FC<SongPageReviewProps> = (props) => {
   const [editedTitle, setEditedTitle] = useState(title);
   const [editedComment, setEditedComment] = useState(comment);
   const [originalRating] = useState(rating); 
+  const [validated, setValidated] = useState(false);
 
   const handleToggleLikeReview = async (reviewId: string) => {
     setIsLoading(true);
@@ -58,20 +61,27 @@ const SongPageReview: React.FC<SongPageReviewProps> = (props) => {
     setIsEditing(true);
   };
 
-  const handleSaveEdit = async () => {
+  const handleSaveEdit = async (e: React.FormEvent<HTMLFormElement> ) => {
 
-    const isRatingEdited = editedRating !== originalRating;
-    const isTitleEdited = editedTitle !== title;
-    const isCommentEdited = editedComment !== comment;
+    e.preventDefault();
+    const form = e.target as HTMLFormElement;
+    if(form.checkValidity() === false) {
+      e.preventDefault();
+      e.stopPropagation();
+    }else {
+      const isRatingEdited = editedRating !== originalRating;
+      const isTitleEdited = editedTitle !== title;
+      const isCommentEdited = editedComment !== comment;
 
-    if (!isRatingEdited && !isTitleEdited && !isCommentEdited) {
+      if (!isRatingEdited && !isTitleEdited && !isCommentEdited) {
+        setIsEditing(false);
+        return;
+      }
+      handleReviewEdit(id, editedRating, editedComment, editedTitle);
+
       setIsEditing(false);
-      return;
     }
-
-    handleReviewEdit(id, editedRating, editedComment, editedTitle);
-
-    setIsEditing(false);
+    setValidated(true);
   };
 
   const handleCancelEdit = () => {
@@ -96,69 +106,98 @@ const SongPageReview: React.FC<SongPageReviewProps> = (props) => {
       setEditedRating(newRating);
     }
   };
-
   return (
     <div key={index} className="col-md-3 mb-3">
       <div className="card">
-        <div className="card-body card-fixed-height">
-          <Link key={index} to={'/user/' + userId}>
-            <h4 className="card-text mt-4">By: {username}</h4>
-          </Link>
+        <div className="card-body song-page-card-min-height">
+          <div className="d-flex">
+            <Link key={index} to={"/user/" + userId}>
+              <h4 className="card-text">By: {username}</h4>
+            </Link>
+            <div className="d-flex justify-content-end ms-3">
+              {userId === loggedUserId && !isEditing && (
+                <span
+                  role="button"
+                  className="material-symbols-outlined"
+                  onClick={handleEdit}
+                >
+                  edit
+                </span>
+              )}
+              {userId === loggedUserId && !isEditing && (
+                <span
+                  role="button"
+                  className="material-symbols-outlined ms-3"
+                  onClick={handleDelete}
+                >
+                  Delete
+                </span>
+              )}
+            </div>
+          </div>
           {isEditing ? (
-  <div>
-    <h4 className="card-text mt-4">Edit Title</h4>
-    <input
-      type="text"
-      value={editedTitle}
-      onChange={(e) => setEditedTitle(e.target.value)}
-      minLength={1}
-      maxLength={50}
-      required
-      style={{
-        border: (editedTitle.length < 1 || editedTitle.length > 50) ? '3px solid red' : '3px solid green',
-      }}
-    />
-    <h4 className="card-text mt-4">Edit Comment</h4>
-    <input
-      type="text"
-      value={editedComment}
-      onChange={(e) => setEditedComment(e.target.value)}
-      minLength={1}
-      maxLength={250}
-      required
-      style={{
-        border: (editedComment.length < 1 || editedComment.length > 250) ? '3px solid red' : '3px solid green',
-      }}
-    />
-    <h4 className="card-text mt-4">Edit Rating</h4>
-    <input
-      type="number"
-      value={editedRating}
-      onChange={handleRatingChange}
-      min="1"
-      max="5"
-      required
-      style={{
-        border: (editedRating < 1 || editedRating > 5) ? '3px solid red' : '3px solid green',
-      }}
-    />
-    <button onClick={handleSaveEdit} disabled={
-      editedTitle.length < 1 || editedTitle.length > 50 ||
-      editedComment.length < 1 || editedComment.length > 250 ||
-      editedRating < 1 || editedRating > 5
-    }>
-      Save
-    </button>
-    <button onClick={handleCancelEdit}>Cancel</button>
-    {userId === loggedUserId && !isEditing && (
-      <button onClick={handleDelete}>Delete</button>
-    )}
-  </div>
-) : (
-            
+            <div>
+              <Form noValidate validated={validated} onSubmit={handleSaveEdit}>
+                <Form.Group className="card-text mt-2" controlId="review-title">
+                  <Form.Label><h5>Title</h5></Form.Label>
+                  <Form.Control
+                    required
+                    type="text"
+                    placeholder="Title"
+                    value={editedTitle}
+                    onChange={(e) => setEditedTitle(e.target.value)}
+                    minLength={5}
+                    maxLength={50}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    Please provide a valid title.
+                  </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group className="card-text mt-2" controlId="review-rating">
+                  <Form.Label><h5>Rating</h5></Form.Label>
+                  <Form.Control
+                    required
+                    type="number"
+                    placeholder="Rating"
+                    value={editedRating}
+                    onChange={handleRatingChange}
+                    min={1}
+                    max={5}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    Please provide a rating between 1 and 5.
+                  </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group className="card-text mt-2" controlId="review-comment">
+                  <Form.Label><h5>Comment</h5></Form.Label>
+                  <Form.Control as ="textarea" rows={5}
+                    required
+                    placeholder="Comment"
+                    value={editedComment}
+                    onChange={(e) => setEditedComment(e.target.value)}
+                    minLength={10}
+                    maxLength={500}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    Please provide a valid comment.
+                  </Form.Control.Feedback>
+                  <Button variant="primary" type="submit" className="mt-2">
+                    Save
+                  </Button>
+                  <Button
+                    variant="danger"
+                    className="mt-2 ms-2"
+                    onClick={handleCancelEdit}
+                  >
+                    Cancel
+                  </Button>
+                </Form.Group>
+              </Form>
+            </div>
+          ) : (
             <div>
               <h4 className="card-text mt-4">
-                {title} {createdAt !== updatedAt? "(edited)" : null}
+                {title}
               </h4>
               <p className="card-text">{comment}</p>
               <h4 className="card-text">{rating}/5</h4>
@@ -166,7 +205,9 @@ const SongPageReview: React.FC<SongPageReviewProps> = (props) => {
                 onClick={() => handleToggleLikeReview(id)}
                 disabled={isLoading}
                 style={{
-                  textShadow: likes.includes(loggedUserId) ? "0px 0px 15px #FF0000" : "none",
+                  textShadow: likes.includes(loggedUserId)
+                    ? "0px 0px 15px #FF0000"
+                    : "none",
                   fontSize: "20px",
                   background: "none",
                   border: "none",
@@ -180,17 +221,12 @@ const SongPageReview: React.FC<SongPageReviewProps> = (props) => {
                   {likes.length}
                 </span>
               </button>
-              {userId === loggedUserId && !isEditing && (
-                <button onClick={handleEdit}>Edit</button>
-              )}
-              {userId === loggedUserId && !isEditing && (
-                <button onClick={handleDelete}>Delete</button>
-              )}
             </div>
           )}
-          <h4 className="card-text">
-            {moment(createdAtDate).fromNow()}
-          </h4>
+          <div className="d-flex">
+            <h4 className="card-text">{moment(createdAtDate).fromNow()}</h4>
+            <p className="text-muted ms-1 mt-1"><small>{createdAt !== updatedAt ? "(edited)" : null}</small></p>
+          </div>
         </div>
       </div>
     </div>
